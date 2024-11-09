05b612498a6e7ba97c1892a94fb3b8143a076ce9
HitarthThakkar <122635451+HitarthThakkar@users.noreply.github.com>
2024-11-09 13:23:35 +0530

    Time: 0 ms (100%), Space: 8.5 MB (82.17%) - LeetHub

--- 3394-minimum-array-end/3394-minimum-array-end.cpp
+++ 3394-minimum-array-end/3394-minimum-array-end.cpp
@@ -3,17 +3,15 @@ class Solution
 public:
     long long minEnd(int n, int x)
     {
-        n--; int i = 63, j = 63; long long int ans = 0, mul = 1;
-        vector<int> binx(64, 0), binnmo(64, 0);
-        while(n) { if(n & 1) binnmo[i] = 1; else binnmo[i] = 0; n /= 2; i--; }
-        i = 63;
-        while(x) { if(x & 1) binx[i] = 1; else binx[i] = 0; x /= 2; i--; }
-        for(int i = 63; i >= 0; i--)
+        long long ans = x, mask; n--;
+        for (mask = 1; n > 0; mask <<= 1)
         {
-            while(j >= 0 && binx[j] != 0) j--;
-            if(j >= 0) binx[j] = binnmo[i], j--; else break;
+            if ((mask & x) == 0)
+            {
+                ans |= (n & 1) * mask;
+                n >>= 1;
+            }
         }
-        for(int i = 63; i >= 2; i--) { ans += (mul * binx[i]); mul *= 2; }
         return ans;
     }
 };
