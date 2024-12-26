b2ac742f43091dfca2db2f25b38c9958d82e63b2
HitarthThakkar <122635451+HitarthThakkar@users.noreply.github.com>
2024-12-26 13:09:29 +0530

    Time: 11 ms (64.63%), Space: 27.3 MB (24.58%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,16 +1,25 @@
 class Solution
 {
 private:
-    int recur(int ind, vector<int> &nums, int target, int val)
+    int recur(int ind, vector<int> &nums, int target, int val, vector<vector<int>> &dp)
     {
-        if(ind == nums.size()) return (val == target);
-        int one = recur(ind + 1, nums, target, val + nums[ind]);
-        int two = recur(ind + 1, nums, target, val - nums[ind]);
-        return (one + two);
+        if(ind == nums.size()) return ((val == target) || (val == (target * -1)));
+        int conval, conval2;
+        conval = (val + nums[ind]); if(conval < 0) conval *= -1;
+        if(dp[ind + 1][conval] == -1)
+            dp[ind + 1][conval] = recur(ind + 1, nums, target, val + nums[ind], dp);
+        conval2 = (val - nums[ind]); if(conval2 < 0) conval2 *= -1;
+        if(dp[ind + 1][conval2] == -1)
+            dp[ind + 1][conval2] = recur(ind + 1, nums, target, val - nums[ind], dp);
+        return (dp[ind + 1][conval] + dp[ind + 1][conval2]);
     }
 public:
     int findTargetSumWays(vector<int>& nums, int target)
     {
-        return recur(0, nums, target, 0);
+        int n = nums.size();
+        vector<vector<int>> dp((n + 1), vector<int> (1001, -1));
+        if(target == 0) return recur(0, nums, target, 0, dp);
+        else if(target < 0) target *= -1;
+        return ((recur(0, nums, target, 0, dp) / 2));
     }
 };
