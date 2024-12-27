a1a99c61d4b69d7b6c5014b5a2ec13d9388bd1cc
HitarthThakkar <122635451+HitarthThakkar@users.noreply.github.com>
2024-12-28 01:30:17 +0530

    Time: 0 ms (100%), Space: 7.7 MB (54.38%) - LeetHub

--- 0397-integer-replacement/0397-integer-replacement.cpp
+++ 0397-integer-replacement/0397-integer-replacement.cpp
@@ -3,12 +3,14 @@ class Solution
 public:
     int integerReplacement(long long n)
     {
-        if(n == 1) return 0;
-        else if(__builtin_popcount(n) == 1) return log2(n);
-        else if(n % 2 == 0) return (integerReplacement(n / 2) + 1);
-        // else if(__builtin_popcount(n + 1) < __builtin_popcount(n - 1))
-        //     return (integerReplacement(n + 1) + 1);
-        else 
-            return (min(integerReplacement(n + 1), integerReplacement(n - 1)) + 1);
+        int ans = 0;
+        while(n != 1)
+        {
+            if(n % 2 == 0) n /= 2;
+            else if(n % 4 == 1 || n == 3) n--;
+            else n++;
+            ans++;
+        }
+        return ans;
     }
 };
