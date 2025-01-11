aa2cfcfc1b1801927fdb7e87ea2d276a61a25719
HitarthThakkar <122635451+HitarthThakkar@users.noreply.github.com>
2025-01-11 10:54:20 +0530

    Time: 63 ms (26.12%), Space: 88.9 MB (11.19%) - LeetHub

---
+++ 3296-minimum-time-to-revert-word-to-initial-state-ii/3296-minimum-time-to-revert-word-to-initial-state-ii.cpp
@@ -0,0 +1,59 @@
+class Solution
+{
+public:
+    int minimumTimeToInitialState(string word, int k)
+    {
+        // cout << word << endl;
+
+        int n = word.length(), j = 0;
+
+        vector<int> lps(n, 0);
+
+        for(int i = 1; i < n; i++)
+        {
+            while(j > 0 && word[j] != word[i]) j = lps[j - 1];
+            if(word[j] == word[i]) lps[i] = ++j;
+        }
+
+        // for(auto ele : lps) cout << ele << " "; cout << endl;
+
+        int lentocut = n - lps[n - 1];
+
+        if(lps[n - 1] + 1 == n)
+        {
+            return 1;
+        }
+
+        if(lentocut == n)
+        {
+            if(n % k == 0) return (n / k);
+            else return ((n / k) + 1);
+        }
+        else if(lentocut == k)
+        {
+            return 1;
+        }
+        else if(lentocut > k)
+        {
+            int actualcut = ((lentocut / k) * k);
+
+            if(actualcut == lentocut)
+            {
+                return (lentocut / k);
+            }
+
+            if(actualcut + k - 1 > n - 1)
+            {
+                return (lentocut / k) + 1;
+            }
+
+            word[actualcut + k - 1] = '#';
+            return minimumTimeToInitialState(word, k);
+        }
+        else
+        {
+            word[k - 1] = '@';
+            return minimumTimeToInitialState(word, k);
+        }
+    }
+};
