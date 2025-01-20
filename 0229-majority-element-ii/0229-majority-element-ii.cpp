4c854b4c4fab678dd391a3396e2f8658a593961a
HitarthThakkar <122635451+HitarthThakkar@users.noreply.github.com>
2025-01-20 17:50:51 +0530

    Time: 0 ms (100%), Space: 19.7 MB (92.02%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -3,60 +3,19 @@ class Solution
 public:
     vector<int> majorityElement(vector<int>& nums)
     {
-        int count1 = 0, count2 = 0;
-        int ele1 = INT_MIN, ele2 = INT_MIN;
-
+        int c1 = 0, c2 = 0, ele1 = INT_MIN, ele2 = INT_MIN, mini = nums.size() / 3;
         for(int i = 0; i < nums.size(); i++)
         {
-            if(count1 == 0 && ele2 != nums[i])
-            {
-                count1++;
-                ele1 = nums[i];
-            }
-            else if(count2 == 0 && ele1 != nums[i])
-            {
-                count2++;
-                ele2 = nums[i];
-            }
-            else if(ele1 == nums[i])
-            {
-                count1++;
-            }
-            else if(ele2 == nums[i])
-            {
-                count2++;
-            }
-            else
-            {
-                count1--;
-                count2--;
-            }
+            if(c1 == 0 && ele2 != nums[i]) c1++, ele1 = nums[i];
+            else if(c2 == 0 && ele1 != nums[i]) c2++, ele2 = nums[i];
+            else if(ele1 == nums[i]) c1++;
+            else if(ele2 == nums[i]) c2++;
+            else c1--, c2--;
         }
-        int c1 = 0, c2 = 0;
+        c1 = 0, c2 = 0;
         for(int i = 0; i < nums.size(); i++)
-        {
-            if(ele1 == nums[i])
-            {
-                c1++;
-            }
-            if(ele2 == nums[i])
-            {
-                c2++;
-            }
-        }
-
-        vector<int> v;
-        int mini = nums.size() / 3;
-
-        if(c1 > mini)
-        {
-            v.push_back(ele1);
-        }
-        if(c2 > mini)
-        {
-            v.push_back(ele2);
-        }
-
+            if(ele1 == nums[i]) c1++; else if(ele2 == nums[i]) c2++;
+        vector<int> v; if(c1 > mini) v.push_back(ele1); if(c2 > mini) v.push_back(ele2);
         return v;
     }
 };
