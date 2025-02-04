ec81cd5657bd06c47798531b3fcc234c04c7da17
HitarthThakkar <122635451+HitarthThakkar@users.noreply.github.com>
2025-02-04 15:53:04 +0530

    Time: 0 ms (100%), Space: 17.9 MB (89.61%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,7 +1,7 @@
 class Solution
 {
-private:
-    bool func(vector<int>& nums, int target)
+public:
+    bool search(vector<int>& nums, int target)
     {
         int n = nums.size();
         int lo = 0;
@@ -9,54 +9,30 @@ private:
 
         if(target == nums[0]) return true;
 
-        while(hi - lo > 1)
+        while(lo <= hi)
         {
             int mid = (lo + hi) / 2;
 
             if(nums[mid] == target) return true;
 
-            if(nums[mid] >= nums[0])
+            if(nums[mid] == nums[lo] && nums[mid] == nums[hi])
             {
-                if(target > nums[0] && target < nums[mid]) hi = mid - 1;
+                lo++; hi--;
+                continue;
+            }
+
+            if(nums[mid] >= nums[lo])
+            {
+                if(target >= nums[lo] && target <= nums[mid]) hi = mid - 1;
                 else lo = mid + 1;
             }
             else
             {
-                if(target < nums[0] && target > nums[mid]) lo = mid + 1;
+                if(nums[mid] <= target && target <= nums[hi]) lo = mid + 1;
                 else hi = mid - 1;
             }
         }
 
-        if(nums[lo] == target) return true;
-        if(nums[hi] == target) return true;
         return false;
-    }
-public:
-    bool search(vector<int>& nums, int target)
-    {
-        int n = nums.size();
-
-        if(nums[0] != nums[n - 1])
-        {
-            return func(nums, target);
-        }
-        else
-        {
-
-            if(nums[0] == target) return true;
-
-            int lo = 1;
-            int hi = n - 2;
-
-            for(int i = 0; i <= hi; i++)
-            {
-                if(nums[i] == target)
-                {
-                    return true;
-                }
-            }
-
-            return false;
-        }
     }
 };
