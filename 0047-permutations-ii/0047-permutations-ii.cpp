190e42cd595c39cf58fed555d0b15d493331bf34
HitarthThakkar <122635451+HitarthThakkar@users.noreply.github.com>
2025-02-08 18:56:45 +0530

    Time: 0 ms (100%), Space: 11.3 MB (96%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,35 +1,26 @@
 class Solution
 {
 public:
-    void recur(int stat, vector<int>& temp, vector<int>& nums, vector<vector<int>>& ans)
+    void recur(int index, vector<int> &nums, vector<vector<int>> &ans)
     {
-        if(temp.size() == nums.size())
+        if (index == nums.size()) ans.push_back(nums);
+        int stat = 0;
+        for (int i = index; i < nums.size(); i++)
         {
-            ans.push_back(temp);
-            return;
-        }
-
-        bool prevTaken = false;
-
-        for(int i = 0; i < nums.size(); i++)
-        {
-            if(i > 0 && nums[i] != nums[i - 1]) prevTaken = false;
-
-            if(((stat & (1 << i)) == 0) && !prevTaken)
+            if((stat & (1 << (nums[i] + 10))) == 0)
             {
-                prevTaken = true;
-                temp.push_back(nums[i]);
-                recur((stat | (1 << i)), temp, nums, ans);
-                temp.pop_back();
+                stat |= (1 << (nums[i] + 10));
+                swap(nums[index], nums[i]);
+                recur(index + 1, nums, ans);
+                swap(nums[index], nums[i]);
             }
         }
     }
-
     vector<vector<int>> permuteUnique(vector<int>& nums)
     {
         sort(nums.begin(), nums.end());
-        vector<vector<int>> ans; vector<int> temp;
-        recur(0, temp, nums, ans);
+        vector<vector<int>> ans;
+        recur(0, nums, ans);
         return ans;
     }
 };
