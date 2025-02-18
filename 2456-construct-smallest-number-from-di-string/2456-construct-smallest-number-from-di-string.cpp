bbdf6527407edacdb0f496f205f70ef196a3ec39
HitarthThakkar <122635451+HitarthThakkar@users.noreply.github.com>
2025-02-18 10:59:39 +0530

    Time: 0 ms (100%), Space: 8.5 MB (27.59%) - LeetHub

--- 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
@@ -1,31 +1,22 @@
 class Solution
 {
-private:
-    bool recur(int ind, char prev, bool sign, map<char, int> &left, string &answer, string pattern)
-    {
-        for(auto &pr : left)
-        {
-            if((pr.second > 0) && ((sign && pr.first > prev) || (!sign && pr.first < prev)))
-            {
-                answer += pr.first;
-                if(ind == pattern.length()) return true;
-                pr.second--;
-                bool newsign = (pattern[ind] == 'D' ? false : true);
-                if(recur(ind + 1, pr.first, newsign, left, answer, pattern)) return true;
-                pr.second++;
-                answer.pop_back();
-            }
-        }
-        return false;
-    }
 public:
     string smallestNumber(string pattern)
     {
-        map<char, int> left;
-        for(char c = '1'; c <= '9'; c++) left.insert({c, 1});
-        bool sign = true;
         string answer = "";
-        recur(0, '0', sign, left, answer, pattern);
+        stack<int> st;
+        for(int i = 0; i < pattern.length() + 1; i++)
+        {
+            st.push(i + 1);
+            if(i >= pattern.length() || pattern[i] == 'I')
+            {
+                while(!st.empty())
+                {
+                    answer += to_string(st.top());
+                    st.pop();
+                }
+            }
+        }
         return answer;
     }
 };
