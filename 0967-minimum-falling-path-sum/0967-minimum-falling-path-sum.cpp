f09524ee9c0dc51c1c8127cee9ccbf4c1e186221
HitarthThakkar <122635451+HitarthThakkar@users.noreply.github.com>
2025-03-08 21:37:25 +0530

    Time: 0 ms (100%), Space: 14.6 MB (80.42%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -5,18 +5,19 @@ public:
     {
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<vector<int>> dp(n, vector<int> (m, -1));
-        for(int j = 0; j < m; j++) dp[0][j] = matrix[0][j];
+        vector<int> prev(m, -1), cur(m, -1);
+        for(int j = 0; j < m; j++) prev[j] = matrix[0][j];
         for(int i = 1; i < n; i++) {
             for(int j = 0; j < m; j++) {
-                int a = dp[i - 1][j];
-                int b = ((j != m - 1) ? dp[i - 1][j + 1] : 1e9);
-                int c = ((j != 0) ? dp[i - 1][j - 1] : 1e9);
-                dp[i][j] = matrix[i][j] + min({a, b, c});
+                int a = prev[j];
+                int b = ((j != m - 1) ? prev[j + 1] : 1e9);
+                int c = ((j != 0) ? prev[j - 1] : 1e9);
+                cur[j] = matrix[i][j] + min({a, b, c});
             }
+            prev = cur;
         }
         int ans = 1e9;
-        for(int j = 0; j < m; j++) ans = min(dp[n - 1][j], ans);
+        for(int j = 0; j < m; j++) ans = min(prev[j], ans);
         return ans;
     }
 };
