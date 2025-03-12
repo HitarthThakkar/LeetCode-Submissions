9be02a14ce6160d4fbaa11fd3b8712eeff4e061b
HitarthThakkar <122635451+HitarthThakkar@users.noreply.github.com>
2025-03-13 04:04:18 +0530

    Time: 0 ms (100%), Space: 8.3 MB (84.28%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.cpp
+++ 0778-reorganize-string/0778-reorganize-string.cpp
@@ -3,16 +3,16 @@ class Solution
 public:
     string reorganizeString(string s)
     {
+        // 2025 Hiring Prep Sprint Q8 [Medium but got intuition way faster]
+        // imma lookup formal proof or similar approach-ness thing though
         vector<int> f(26, 0); multimap<int, char> mp;
         for(auto c : s) f[c - 'a']++;
         for(int i = 0; i < 26; i++) if(f[i] > 0) mp.insert({f[i], i + 'a'});
         auto it = --mp.end(); int dc = 0; int i = 0; int j = 1; string ans = s;
         while(dc < s.length())
         {
-            // cout << i << " " << j << " " << dc << " " << ans << endl;
             char ch = it->second;
             int f = it->first;
-            // cout << ch << " " << f << endl;
             while(f--)
             {
                 dc++;
@@ -22,6 +22,7 @@ public:
             }
             it--;
         }
-        for(int i = 1; i < s.length(); i++) if(ans[i - 1] == ans[i]) return ""; return ans;
+        for(int i = 1; i < s.length(); i++)
+            if(ans[i - 1] == ans[i]) return ""; return ans;
     }
 };
