b4517904ba3dd06fd0f61ccbf2e46723dfce6c66
HitarthThakkar <122635451+HitarthThakkar@users.noreply.github.com>
2025-03-13 02:16:41 +0530

    Time: 2809 ms (5.01%), Space: 17.1 MB (21.88%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
@@ -3,20 +3,22 @@ class Solution
 public:
     int countPalindromicSubsequence(string s)
     {
-        unordered_map<char, pair<int, int>> mp;
+        // 2025 Hiring Prep Sprint Q5 [Medium, actually]
+        vector<int> pfx(26, 0), sfx(26, 0);
+        for(auto ele : s) sfx[ele - 'a']++;
+        set<string> ans;
         for(int i = 0; i < s.length(); i++)
         {
-            if(mp.find(s[i]) == mp.end()) mp.insert({s[i], {i, -1}});
-            else mp[s[i]].second = i;
+            sfx[s[i] - 'a']--;
+            for(int j = 0; j < 26; j++)
+                if(pfx[j] > 0 && sfx[j] > 0)
+                {
+                    char ch = (char) (j + 'a');
+                    string ss = ""; ss += ch; ss += s[i]; ss += ch;
+                    ans.insert(ss);
+                }
+            pfx[s[i] - 'a']++;
         }
-        int ans = 0;
-        for(auto ele : mp)
-        {
-            unordered_set<char> set;
-            for(int i = ele.second.first + 1; i < ele.second.second; i++)
-                set.insert(s[i]);
-            ans += set.size();
-        }
-        return ans;
+        return ans.size();
     }
 };
