b7e3ecafaea399663d60ec60ce3383629d586332
HitarthThakkar <122635451+HitarthThakkar@users.noreply.github.com>
2025-03-13 03:30:30 +0530

    Time: 7 ms (19.61%), Space: 14.9 MB (23.04%) - LeetHub

--- 1483-rank-teams-by-votes/1483-rank-teams-by-votes.cpp
+++ 1483-rank-teams-by-votes/1483-rank-teams-by-votes.cpp
@@ -1,38 +1,15 @@
 class Solution
 {
-private:
-    string topoSort(int n, vector<vector<int>> &adj, set<char> &valid)
-    {
-        // 2025 Hiring Prep Sprint Q6 [Very Good Medium]
-        vector<int> indegree(n, 0);
-        for (int i = 0; i < n; i++)
-            if(valid.find('A' + i) != valid.end())
-                for (auto nei : adj[i]) indegree[nei]++;
-        queue<int> q;
-        for (int i = 0; i < n; i++)
-            if (valid.find('A' + i) != valid.end() && indegree[i] == 0)
-                q.push(i);
-        string res = "";
-        while (!q.empty())
-        {
-            int node = q.front();
-            q.pop();
-            res += (char) ('A' + node);
-            for (auto nei : adj[node])
-            {
-                indegree[nei]--;
-                if (indegree[nei] == 0) q.push(nei);
-            }
-        }
-        return res;
-    }
 public:
     string rankTeams(vector<string>& votes)
     {
+        // 2025 Hiring Prep Sprint Q6 [Very Good Medium]
+        // Previous soln was using toposort, 
+        // but it's actually costly and redundant. we don't need it. WE ARE SMART.
         int n = votes.size();
         int len = votes[0].size();
         vector<vector<int>> v(26, vector<int> (len, 0));
-        vector<vector<int>> adj(26);
+        vector<int> ctr(26, 0);
         set<char> valid;
         for(auto ch : votes[0]) valid.insert(ch);
         for(auto s : votes) for(int i = 0; i < len; i++) v[s[i] - 'A'][i]++;
@@ -46,12 +23,24 @@ public:
                     {
                         int k = 0;
                         while(k < len && v[i][k] == v[j][k]) k++;
-                        if(k == len || v[i][k] > v[j][k]) adj[i].push_back(j);
-                        else if(v[i][k] < v[j][k]) adj[j].push_back(i);
+                        if(k == len || v[i][k] > v[j][k]) ctr[i]++;
+                        else if(v[i][k] < v[j][k]) ctr[j]++;
                     }
                 }
             }
         }
-        return topoSort(26, adj, valid);
+        map<int, char> order;
+        for(int i = 0; i < 26; i++)
+        {
+            if(valid.find('A' + i) != valid.end())
+            {
+                char ch = (char) ('A' + i);
+                order[ctr[i]] = ch;
+            }
+        }
+        string result = "";
+        for(auto ele : order) result += ele.second;
+        reverse(result.begin(), result.end());
+        return result;
     }
 };
