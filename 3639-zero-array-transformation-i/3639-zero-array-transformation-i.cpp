bb4c8e80e219d0ca85740e032f1beadc100a2f4a
HitarthThakkar <122635451+HitarthThakkar@users.noreply.github.com>
2025-03-13 13:19:34 +0530

    Time: 151 ms (17.15%), Space: 325.5 MB (25.57%) - LeetHub

---
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries)
+    {
+        int n = nums.size();
+        vector<int> pfx(n, 0);
+        for(auto q : queries) {
+            pfx[q[0]]--;
+            if(q[1] < n - 1) pfx[q[1] + 1]++;
+        }
+        for(int i = 1; i < n; i++) pfx[i] += pfx[i - 1];
+        for(int i = 0; i < n; i++) if(nums[i] + pfx[i] > 0) return false;
+        return true;
+    }
+};
