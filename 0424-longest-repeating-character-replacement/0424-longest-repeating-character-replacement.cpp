3834a8d2a22eba75eb569b5bf2528ceba2f34ad5
HitarthThakkar <122635451+HitarthThakkar@users.noreply.github.com>
2025-03-16 11:39:57 +0530

    Time: 29 ms (8.87%), Space: 10.8 MB (93.31%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+    int characterReplacement(string s, int k)
+    {
+        vector<int> f(26, 0);
+        int left = 0, right = 0, ans = 0;
+        while(right < s.length())
+        {
+            f[s[right] - 'A']++;
+            int mex = *max_element(f.begin(), f.end());
+            int sum = accumulate(f.begin(), f.end(), 0);
+            while(sum - mex > k)
+            {
+                f[s[left++] - 'A']--;
+                mex = *max_element(f.begin(), f.end());
+                sum = accumulate(f.begin(), f.end(), 0);
+            }
+            ans = max(ans, right - left + 1);
+            right++;
+        }
+        return ans;
+    }
+};
