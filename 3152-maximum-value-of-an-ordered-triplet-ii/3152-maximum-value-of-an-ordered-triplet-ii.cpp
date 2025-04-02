5e56077b18f387153422d7742367a1f98ec92bde
HitarthThakkar <122635451+HitarthThakkar@users.noreply.github.com>
2025-04-02 13:49:22 +0530

    Time: 0 ms (100%), Space: 91.1 MB (78.3%) - LeetHub

--- 3152-maximum-value-of-an-ordered-triplet-ii/3152-maximum-value-of-an-ordered-triplet-ii.cpp
+++ 3152-maximum-value-of-an-ordered-triplet-ii/3152-maximum-value-of-an-ordered-triplet-ii.cpp
@@ -3,16 +3,17 @@ class Solution
 public:
     long long maximumTripletValue(vector<int>& nums)
     {
+        // Overpowered Solution, defeated me fr...
+        // DRY RUN THIS PLZ, THIS IS OVERPOWERED.
+        // WATCH SUBMISSIONS HISTORY & REALIZE GREATNESS OF THIS QUE.
         int n = nums.size();
-        vector<int> rightmax(n, nums[n - 1]);
-        for(int i = n - 2; i >= 0; i--) rightmax[i] = max(nums[i], rightmax[i + 1]);
-        long long leftmax = nums[0];
-        long long answer = 0;
-        for(int j = 1; j < n - 1; j++)
+        long long res = 0, imax = 0, dmax = 0;
+        for (int k = 0; k < n; k++)
         {
-            answer = max(answer, (leftmax - nums[j]) * 1LL * rightmax[j + 1]);
-            leftmax = max(leftmax, 1LL * nums[j]);
+            res = max(res, dmax * nums[k]);     // nums[k] represents nums[k]
+            dmax = max(dmax, imax - nums[k]);   // nums[k] represents nums[j]
+            imax = max(imax, 1LL * nums[k]);    // nums[k] represents nums[i]
         }
-        return answer;
+        return res;
     }
 };
