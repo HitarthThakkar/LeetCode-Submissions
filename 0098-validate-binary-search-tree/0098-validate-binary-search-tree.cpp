eee68b03ae2c459fe3b5e62de43642bff6377476
HitarthThakkar <122635451+HitarthThakkar@users.noreply.github.com>
2025-04-12 22:02:09 +0530

    Time: 0 ms (100%), Space: 21.7 MB (94.97%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -1,72 +1,20 @@
+#define ll long long int
 class Solution
 {
-private:
-    pair<int, int> recur(TreeNode* node, bool &answer)
-    {
-        if(answer == false) return {-1, -1};
-
-        pair<int, int> lp, rp;
-        bool left = false, right = false;
-
-        if(node->left)
-        {
-            left = true;
-            lp = recur(node->left, answer);
-            if(answer == false) return {-1, -1};
-        }
-        if(node->right)
-        {
-            right = true;
-            rp = recur(node->right, answer);
-            if(answer == false) return {-1, -1};
-        }
-
-        if(left == false && right == false)
-        {
-            return {node->val, node->val};
-        }
-        else if(left == false && right == true)
-        {
-            if(min(rp.first, rp.second) > node->val)
-            {
-                return {node->val, max(rp.first, rp.second)};
-            }
-            else
-            {
-                answer = false;
-                return {-1, -1};
-            }
-        }
-        else if(left == true && right == false)
-        {
-            if(max(lp.first, lp.second) < node->val)
-            {
-                return {min(lp.first, lp.second), node->val};
-            }
-            else
-            {
-                answer = false;
-                return {-1, -1};
-            }
-        }
-        else
-        {
-            if((min(rp.first, rp.second) > node->val) && (max(lp.first, lp.second) < node->val))
-            {
-                return {min(lp.first, lp.second), max(rp.first, rp.second)};
-            }
-            else
-            {
-                answer = false;
-                return {-1, -1};
-            }
-        }
-    }
 public:
+    bool RangeBasedRecur(TreeNode* node, ll mini, ll maxi)
+    {
+        if(!node) return true;
+        if(node->val > mini && node->val < maxi)
+        {
+            bool left = RangeBasedRecur(node->left, mini, node->val);
+            bool right = RangeBasedRecur(node->right, node->val, maxi);
+            return (left & right);
+        }
+        return false;
+    }
     bool isValidBST(TreeNode* root)
     {
-        bool answer = true;
-        recur(root, answer);
-        return answer;
+        return RangeBasedRecur(root, -1e16, 1e16);
     }
 };
