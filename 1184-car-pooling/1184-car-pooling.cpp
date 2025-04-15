7c7d46b045f3688a0f26f262b03355172b3598d6
HitarthThakkar <122635451+HitarthThakkar@users.noreply.github.com>
2025-04-15 12:47:16 +0530

    Time: 0 ms (100%), Space: 14.8 MB (20.54%) - LeetHub

--- 1184-car-pooling/1184-car-pooling.cpp
+++ 1184-car-pooling/1184-car-pooling.cpp
@@ -3,13 +3,15 @@ class Solution
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity)
     {
-        vector<int> pfx(1001, 0);
+        int mex = -1;
+        for(auto v : trips) mex = max(mex, v[2]);
+        vector<int> pfx(mex + 1, 0);
         for(auto v : trips)
         {
             pfx[v[1]] += v[0];
             pfx[v[2]] -= v[0];
         }
-        for(int i = 1; i < 1001; i++) pfx[i] += pfx[i - 1];
+        for(int i = 1; i < pfx.size(); i++) pfx[i] += pfx[i - 1];
         for(auto e : pfx) if(e > capacity) return false;
         return true;
     }
