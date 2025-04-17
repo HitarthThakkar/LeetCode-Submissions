d9937f53ea5c33de8a52427e396b87a6db319687
HitarthThakkar <122635451+HitarthThakkar@users.noreply.github.com>
2025-04-17 20:03:39 +0530

    Time: 22 ms (13.22%), Space: 14.9 MB (10.45%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -6,26 +6,15 @@ public:
         int n = nums.size();
         sort(nums.begin(), nums.end());
         vector<vector<int>> dp(n);
+        vector<int> ans;
         for(int i = 0; i < n; i++)
         {
             for(int j = i - 1; j >= 0; j--)
-            {
-                if(nums[i] % nums[j] == 0)
-                {
-                    if(dp[i].size() < dp[j].size() + 1)
-                    {
-                        dp[i] = dp[j];
-                        dp[i].push_back(nums[i]);
-                    }
-                }
-            }
-            if(dp[i].size() == 0)
-            {
-                dp[i].push_back(nums[i]);
-            }
+                if(nums[i] % nums[j] == 0 && dp[i].size() < dp[j].size() + 1)
+                    dp[i] = dp[j];
+            dp[i].push_back(nums[i]);
+            if(dp[i].size() > ans.size()) ans = dp[i];
         }
-        vector<int> ans;
-        for(auto e : dp) if(e.size() > ans.size()) ans = e;
         return ans;
     }
 };
