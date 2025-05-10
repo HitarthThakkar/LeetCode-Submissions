48b6c49872362fb0d1b97226005ef56d3cdfcc26
HitarthThakkar <122635451+HitarthThakkar@users.noreply.github.com>
2025-05-10 16:59:38 +0530

    Time: 140 ms (25.08%), Space: 135.5 MB (50.17%) - LeetHub

--- 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
+++ 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
@@ -5,33 +5,18 @@ public:
     {
         long long s1 = 0, s2 = 0;
         int z1 = 0, z2 = 0;
-        for(auto e : nums1)
-        {
-            if(e == 0) z1++;
-            else s1 += e;
+        for(auto &e : nums1) if(e == 0) z1++; else s1 += e;
+        for(auto &e : nums2) if(e == 0) z2++; else s2 += e;
+        if(z1 == 0 && z2 == 0) {
+            if(s1 == s2) return s1; else return -1;
         }
-        for(auto e : nums2)
-        {
-            if(e == 0) z2++;
-            else s2 += e;
+        else if(z1 == 0) {
+            if(s2 + z2 > s1) return -1; return s1;
         }
-        if(z1 == 0 && z2 == 0)
-        {
-            if(s1 == s2) return s1;
-            else return -1;
+        else if(z2 == 0) {
+            if(s1 + z1 > s2) return -1; return s2;
         }
-        else if(z1 == 0)
-        {
-            if(s2 + z2 > s1) return -1;
-            return s1;
-        }
-        else if(z2 == 0)
-        {
-            if(s1 + z1 > s2) return -1;
-            return s2;
-        }
-        else
-        {
+        else {
             return max(s1 + z1, s2 + z2);
         }
         return -1;
