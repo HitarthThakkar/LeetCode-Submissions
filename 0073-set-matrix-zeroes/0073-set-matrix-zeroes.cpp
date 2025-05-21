cb9b786be2c68125e40c412aafd6587c5e588902
HitarthThakkar <122635451+HitarthThakkar@users.noreply.github.com>
2025-05-21 06:28:44 +0530

    Time: 0 ms (100%), Space: 18.5 MB (78.87%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -12,7 +12,6 @@ public:
                 if(matrix[i][j] == 0) matrix[0][j] = 0, matrix[i][0] = 0;
         for(int i = 1; i < m; i++)
             if(matrix[i][0] == 0) for(int j = 0; j < n; j++) matrix[i][j] = 0;
-
         for(int i = 1; i < n; i++)
             if(matrix[0][i] == 0) for(int j = 0; j < m; j++) matrix[j][i] = 0;
         if(fc) for(int i = 0; i < m; i++) matrix[i][0] = 0;
