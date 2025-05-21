f3292a95cf3a3c0966948ed8fe05ad84cfffbfd1
HitarthThakkar <122635451+HitarthThakkar@users.noreply.github.com>
2025-05-21 09:32:40 +0530

    Time: 0 ms (100%), Space: 7.8 MB (86.08%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -2,10 +2,27 @@ class Solution
 {
 public:
     // Cycle detection algo
-    // We can also use map to detect all previous 'n' and when we visit smth
+    // We can also use map/set to detect all previous 'n' and when we visit smth
     // previously visited then we say got cycle and false
     // that is good in the runtime way but requires memory
     // here memory is nothing, maybe require some time more than that.
+
+    /*
+        Let’s say \U0001d45b has \U0001d451 digits. Then the maximum value of \U0001d453(\U0001d45b) occurs when each digit is 9.
+        The square of 9 is 81 so, with \U0001d451 digits:
+            \U0001d453(\U0001d45b) ≤ (81 * \U0001d451)
+        But:
+            The number of digits \U0001d451 is at most log_10(\U0001d45b) + 1.
+
+        So \U0001d453(\U0001d45b) ≤ 81⋅(log_10(\U0001d45b) + 1)
+
+        This grows much more slowly than \U0001d45b. So as \U0001d45b → ∞, \U0001d453(\U0001d45b) ≪ \U0001d45b.
+
+        Conclusion: Large values of \U0001d45b are immediately reduced to much smaller values
+                    after a few iterations.
+
+    */
+
     int digitSquareSum(int n)
     {
         int sum = 0;
