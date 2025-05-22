4b7a832a040af9ac727a952e83ff1222ba43d935
HitarthThakkar <122635451+HitarthThakkar@users.noreply.github.com>
2025-05-22 09:49:59 +0530

    Time: 5 ms (26.05%), Space: 17.8 MB (91.56%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -0,0 +1,32 @@
+#define pai pair<int, int>
+class Solution
+{
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k)
+    {
+        map<int, int> mp;
+        for(auto e : nums) mp[e]++;
+        priority_queue<pai, vector<pai>, greater<pai>> pq;
+        while(k--)
+        {
+            pq.push({mp.begin()->second, mp.begin()->first});
+            mp.erase(mp.begin());
+        }
+        while(!mp.empty())
+        {
+            if(pq.top().first < mp.begin()->second)
+            {
+                pq.pop();
+                pq.push({mp.begin()->second, mp.begin()->first});
+            }
+            mp.erase(mp.begin());
+        }
+        vector<int> ans;
+        while(!pq.empty())
+        {
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;
+    }
+};
