647030075d4134719a9bd4226d6219c78ed625e8
HitarthThakkar <122635451+HitarthThakkar@users.noreply.github.com>
2025-05-24 17:04:59 +0530

    Time: 3 ms (50.72%), Space: 65.2 MB (33.13%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
@@ -3,46 +3,35 @@ class Solution
 public:
     int numSubarrayProductLessThanK(vector<int>& nums, int k)
     {
-        // 2025 Hiring Prep Sprint Week_2 Q3
-
         int n = nums.size();
         int left = 0;
         int right = 0;
+        int count = 0;
         long long int mul = 1;
-
-        vector<pair<int, int>> vp;
 
         while(right < n)
         {
-            mul *= nums[right];
-            if(mul >= k)
+            if(nums[right] >= k)
             {
-                if(left < right) vp.push_back({left, right - 1});
-                while(left <= right && mul >= k) mul /= nums[left++];
+                mul = 1;
+                left = right + 1;
+                right++;
+                continue;
             }
+
+            mul *= nums[right];
+
+            while(mul >= k)
+            {
+                mul /= nums[left];
+                left++;
+            }
+
+            count += (right - left + 1);
+
             right++;
         }
 
-        if(left < right) vp.push_back({left, right - 1});
-
-        int answer = 0;
-        int prevend = -1;
-
-        for(int i = 0; i < vp.size(); i++)
-        {
-            int a = vp[i].first;
-            int b = vp[i].second;
-            int l = b - a + 1;
-            answer += (l * (l + 1) / 2);
-            if(prevend >= a)
-            {
-                int ll = prevend - a + 1;
-                int overlapping = (ll * (ll + 1) / 2);
-                answer -= overlapping;
-            }
-            prevend = b;
-        }
-
-        return answer;
+        return count;
     }
 };
