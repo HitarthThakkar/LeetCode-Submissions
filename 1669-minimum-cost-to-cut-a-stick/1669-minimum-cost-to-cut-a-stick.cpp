480c115ef2e9e24ec3d3a76fb1a2186923918b46
HitarthThakkar <122635451+HitarthThakkar@users.noreply.github.com>
2025-05-29 12:06:34 +0530

    Time: 23 ms (66.47%), Space: 13.8 MB (87.65%) - LeetHub

--- 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
@@ -1,46 +1,41 @@
 class Solution
 {
 public:
-    int recur(int i, int j, vector<int> &cuts, vector<vector<int>> &dp)
-    {
-        if(i > j)
-        {
-            // cout <<  i << " " << j << " entered and returned 0." << endl;
-            return 0;
-        }
-        else
-        {
-            // cout <<  i << " " << j << " entered ..." << endl;
-            if(dp[i][j] != -1)
-            {
-                // cout << "   AND FOUND OUT VISITED !!! VALUE IS : " << dp[i][j] << endl;
-                return dp[i][j];
-            }
-        }
-
-        int mini = 1e9;
-        int abhi_K_abhi = cuts[j + 1] - cuts[i - 1];
-
-        // cout << "   Abhi K abhi " << abhi_K_abhi << " paisa nikal shane" << endl;
-
-        for(int k = i; k <= j; k++)
-        {
-            int cost = abhi_K_abhi + recur(i, k - 1, cuts, dp) + recur(k + 1, j, cuts, dp);
-            mini = min(mini, cost);
-        }
-
-        // cout << i << " " << j << " returned " << mini << endl;
-
-        return dp[i][j] = mini;
-    }
-
     int minCost(int n, vector<int>& cuts)
     {
+        // This is not readable, see previous submission.
+
         cuts.push_back(0);
         cuts.push_back(n);
         sort(cuts.begin(), cuts.end());
         int nn = cuts.size();
-        vector<vector<int>> dp(nn, vector<int> (nn, -1));
-        return recur(1, nn - 2, cuts, dp);
+        vector<vector<int>> dp(nn - 1, vector<int> (nn - 2, 0));
+
+        for(int i = 1; i < nn - 1; i++)
+            dp[i - 1][i - 1] = cuts[i + 1] - cuts[i - 1];
+
+        for(int i = nn - 4; i >= 0; i--)
+        {
+            for(int j = i + 1; j < nn - 2; j++)
+            {
+                int mini = 1e9;
+                int tba = 0;
+                if(i >= 0 && j + 2 < nn) tba = cuts[j + 2] - cuts[i];
+                for(int k = i; k <= j; k++)
+                {
+                    int cost = 0;
+                    int r = i; int c = k - 1;
+                    if(r >= 0 && c >= 0 && r < dp.size() && c < dp[0].size())
+                        cost += dp[r][c];
+                    r = k + 1; c = j;
+                    if(r >= 0 && c >= 0 && r < dp.size() && c < dp[0].size())
+                        cost += dp[r][c];
+                    mini = min(mini, cost);
+                }
+                dp[i][j] = mini + tba;
+            }
+        }
+
+        return dp[0][nn - 3];
     }
 };
