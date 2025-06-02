61a4b649b89640345594019b307c59f94fe97416
HitarthThakkar <122635451+HitarthThakkar@users.noreply.github.com>
2025-06-02 09:08:25 +0530

    Time: 0 ms (100%), Space: 8.6 MB (53.71%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,49 +1,70 @@
-class Solution {
+class Solution
+{
 public:
-    int reverse(int x) {
-        
-        if(x > 2147483647 || x <= -2147483648)
-        {
-            return 0;
-        }
+    int reverse(int x)
+    {
+        /*
+            Intended Approach
 
-        bool neg = false;
-        if(x < 0)
-        {
-            neg = true;
-            x *= -1;
-        }
-        
-        int arr[32];
-        int i = 31;
-        while(x != 0)
-        {
-            arr[i] = x % 10;
+            To "pop" and "push" digits without the help of some auxiliary stack/array,
+            we can use math.
+
+            # pop operation
+            pop = x % 10;
             x /= 10;
-            i--;
-        }
-        i++;
 
-        long long int k = 0;
-        long long int mul = 1;
-        for(int j = i; j < 32; j++)
-        {
-            k += (arr[j] * mul);
-            mul *= 10;
-        }
+            # push operation:
+            temp = rev * 10 + pop;
+            rev = temp;
 
-        if(k > 2147483647)
-        {
-            return 0;
-        }
+            # However, this approach is dangerous, because the statement 
+            temp = rev * 10 + pop can cause overflow.
 
-        if(neg)
+            Luckily, it is easy to check beforehand whether or this statement 
+            would cause an overflow.
+
+            To explain, lets assume that rev is positive.
+
+            If temp = rev * 10 + pop causes overflow, then it must be that
+            rev /geq 10 * INTMAX
+
+            If rev greater than 10 * INTMAX, then we are guarenteed to overflow.
+            BUT WHAT IF rev == 10 * INTMAX ??
+
+            Analyze :
+            We know INTMAX = 2147483647
+            INTMAX / 10 = 214748364 {We do floor here its integer division}
+            so now, 
+            IF rev < 214748364, you're safe no matter the pop (0–9).
+            IF rev > 214748364, you'll definitely overflow.
+            Now the critical case:
+            If rev == 214748364, then the next digit pop must be carefully checked.
+            temp = 214748364 * 10 + pop
+
+            Now, what is the maximum digit we can add without exceeding INT_MAX?
+            2147483640 + pop ≤ 2147483647 ⇒ pop ≤ 7
+            So if pop > 7, the total exceeds INT_MAX, and overflow occurs.
+
+            SAME WE CAN DO FOR NEGATIVE BOUNDARY IN WHICH IF POP IS LESS THAN -8 WE
+            WILL OVERFLOW.
+        */
+
+        int rev = 0;
+
+        while (x != 0)
         {
-            return (k * -1);
+            int pop = x % 10;
+            x /= 10;
+
+            if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && pop > 7))
+                return 0;
+
+            if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && pop < -8))
+                return 0;
+
+            rev = rev * 10 + pop;
         }
-        else
-        {
-            return k;
-        }
+        
+        return rev;
     }
 };
