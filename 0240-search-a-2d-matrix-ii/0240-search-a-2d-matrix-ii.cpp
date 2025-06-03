598426b6417eafdb7b6a1e1af8c4b28bebaaf71b
HitarthThakkar <122635451+HitarthThakkar@users.noreply.github.com>
2025-06-03 15:43:25 +0530

    Time: 51 ms (68.37%), Space: 18.6 MB (66.82%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -1,16 +1,22 @@
-class Solution
-{
+class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target)
-    {
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m = matrix.size();
         int n = matrix[0].size();
         int r = 0, c = n - 1;
-        while(r < m && c >= 0)
-        {
+        // why r = 0 and c = n - 1 ?
+        // cuz 15 ani row ma sauthi moto che & col ma sauthi nano che
+        // aa property bija koi element pase nathi and aa j property useful che.
+        while(r < m && c >= 0) {
             if(matrix[r][c] == target) return true;
             else if(matrix[r][c] > target) c--;
             else r++;
+            /*
+                1) aa col ma sauthi nano hu j chu bija marathi mota che
+                    so aa col ma `target` nathi fr.
+                2) aa row ma sauthi moto hu j chu bija marathi nana che
+                    so aa row ma `target` nathi fr fr.
+            */
         }
         return false;
     }
