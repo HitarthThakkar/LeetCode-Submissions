1a05d211d1a84c7a16c2ce72e223ee8b3eaea0da
HitarthThakkar <122635451+HitarthThakkar@users.noreply.github.com>
2025-06-18 22:56:33 +0530

    Time: 3 ms (38.6%), Space: 40.2 MB (58.95%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -4,25 +4,15 @@ public:
     vector<int> productExceptSelf(vector<int>& nums)
     {
         int n = nums.size();
-        vector<int> pfx, sfx;
-        int prev = 1, prev2 = 1;
-        for(int i = 0; i < n; i++)
-        {
-            pfx.push_back(prev * nums[i]);
-            sfx.push_back(prev2 * nums[n - i - 1]);
-            prev = pfx.back();
-            prev2 = sfx.back();
-        }
-        // for(auto e : pfx) cout << e << " "; cout << endl;
-        // for(auto e : sfx) cout << e << " ";
-        vector<int> res;
-        for(int i = 0; i < n; i++)
-        {
-            int ans = 1;
-            if(i > 0) ans *= pfx[i - 1];
-            if(i < n - 1) ans *= sfx[n - i - 2];
-            res.push_back(ans);
-        }
-        return res;
+        vector<int> ans = nums;
+        // ans behaves as sfx also
+        for(int i = n - 2; i > 0; i--) ans[i] *= ans[i + 1];
+        // ans[0] is ans[1], as we know that for 0th index answer would be [1, n]'s products
+        ans[0] = ans[1];
+        int mul = 1; // pfx multiplication on the fly
+        for(int i = 1; i < n - 1; i++) mul*=nums[i - 1], ans[i] = (mul * ans[i + 1]);
+        // similar to 0th index case calculating seperately
+        ans[n - 1] = mul * nums[n - 2];
+        return ans;
     }
 };
