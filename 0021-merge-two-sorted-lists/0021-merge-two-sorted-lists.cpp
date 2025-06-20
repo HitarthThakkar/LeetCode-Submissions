53c3fd16230fc74431a266573ba836cc6d0ee954
HitarthThakkar <122635451+HitarthThakkar@users.noreply.github.com>
2025-06-20 20:25:27 +0530

    Time: 1 ms (6.43%), Space: 19.6 MB (29.36%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -3,21 +3,25 @@ class Solution
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2)
     {
-        if(list1 == NULL) return list2;
-        if(list2 == NULL) return list1;
-        if(list1->val > list2->val) std::swap(list1,list2);
-        ListNode* ans = list1;
-        while(list1 != NULL && list2 != NULL)
+        ListNode *dummy, *temp;
+        dummy = new ListNode();
+        temp = dummy;
+        while(list1 && list2)
         {
-            ListNode* tmp = NULL;
-            while(list1 != NULL && list1->val <= list2->val)
+            if(list1->val < list2->val)
             {
-                tmp = list1;
+                temp->next = list1;
                 list1 = list1->next;
             }
-            tmp->next = list2;
-            std :: swap(list1,list2);
+            else
+            {
+                temp->next = list2;
+                list2 = list2->next;
+            }
+            temp = temp->next;
         }
-        return ans;
+        if(list1) temp->next = list1;
+        if(list2) temp->next = list2;
+        return dummy->next;
     }
 };
