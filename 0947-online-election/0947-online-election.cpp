64ed1112e50d4672e263bbe07f0d0340a12e1f6d
Hitarth Thakkar <122635451+HitarthThakkar@users.noreply.github.com>
2025-08-09 18:19:01 +0530

    Time: 46 ms (43.91%), Space: 114.2 MB (22.75%) - LeetHub

--- 0947-online-election/0947-online-election.cpp
+++ 0947-online-election/0947-online-election.cpp
@@ -1,32 +1,28 @@
-class TopVotedCandidate
-{
-private:
-    map<int,int> win_at_time;
+/*
+last one was good but this might be a strategic improvement
+[if u think clearly]
+*/
+
+class TopVotedCandidate {
 public:
-    TopVotedCandidate(vector<int>& persons, vector<int>& times)
-    {
-        int n = persons.size();
-        int mxVotes = 0;
-        int winner = 0;
+    unordered_map<int, int> mpp;
+    vector<int> times;
+    TopVotedCandidate(vector<int>& persons, vector<int>& times) {
+        int n = times.size(), lead = -1;
+        this->times = times;
 
-        vector<int> voteCount(n,0);
-
-        for(int i = 0; i < n; i++)
-        {
-            voteCount[persons[i]]++;
-            if(voteCount[persons[i]] >= mxVotes)
-            {
-                winner = persons[i];
-                mxVotes = voteCount[persons[i]];
+        unordered_map<int, int> cnt;
+        for(int i = 0; i < n; i++){
+            cnt[persons[i]]++;
+            if(cnt[persons[i]] >= cnt[lead]){
+                lead = persons[i];
             }
-            win_at_time[times[i]] = winner;
+            mpp[times[i]] = lead;
         }
+
     }
     
-    int q(int t)
-    {
-        auto itr = win_at_time.upper_bound(t);
-        itr--;
-        return itr->second;
+    int q(int t) {
+        return mpp[*--upper_bound(times.begin(), times.end(), t)];
     }
 };
