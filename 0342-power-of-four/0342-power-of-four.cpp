f2a90e30b3bcf91ce70303f90ba99ac56f6ee5f7
Hitarth Thakkar <122635451+HitarthThakkar@users.noreply.github.com>
2025-08-15 10:52:21 +0530

    Time: 0 ms (100%), Space: 7.8 MB (82.36%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -4,7 +4,7 @@ public:
     bool isPowerOfFour(int n)
     {
         if (n <= 0) return false;
-        int mask = 0b01010101010101010101010101010101;
-        return (((n & (n - 1)) == 0) && ((n & mask) == n));
+        if((n & (n - 1)) != 0) return false;
+        return ((n - 1) % 3 == 0);
     }
 };
