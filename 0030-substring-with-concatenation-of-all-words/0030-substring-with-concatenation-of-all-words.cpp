be1dd5cc316f20affda1f6c3ea04695763eba67a
Hitarth Thakkar <122635451+HitarthThakkar@users.noreply.github.com>
2025-08-29 23:54:02 +0530

    Time: 311 ms (16.61%), Space: 111.8 MB (13.46%) - LeetHub

---
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
@@ -0,0 +1,54 @@
+class Solution
+{
+public:
+    vector<int> findSubstring(string s, vector<string>& words)
+    {
+        int word_count = words.size();
+        int word_length = words[0].length();
+        int total_length = word_count * word_length;
+        int string_length = s.length();
+
+        unordered_map<string, int> mp;
+        for(auto word : words) mp[word]++;
+
+        vector<int> result;
+
+        for(int start = 0; start < word_length; start++)
+        {
+            int current_matches = 0;
+            int left = start, right = start;
+            unordered_map<string, int> mpp = mp;
+
+            while((left <= string_length - total_length) && (right < string_length))
+            {
+                string ss = s.substr(right, word_length);
+
+                if(mpp.find(ss) != mpp.end())
+                {
+                    mpp[ss]--;
+                    if(mpp[ss] >= 0) current_matches++;
+                    if(current_matches == word_count) result.push_back(left);
+                    right += word_length;
+
+                    if(right - left >= total_length)
+                    {
+                        string sss = s.substr(left, word_length);
+                        left += word_length;
+                        mpp[sss]++;
+                        if(mpp[sss] > 0) current_matches--;
+                        if(current_matches == word_count) result.push_back(left);
+                    }
+                }
+                else
+                {
+                    mpp = mp;
+                    current_matches = 0;
+                    right += word_length;
+                    left = right;
+                }
+            }
+        }
+
+        return result;
+    }
+};
