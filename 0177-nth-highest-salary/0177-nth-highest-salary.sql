f4ae66d73a53b1da919719314319ca7dad6152c1
Hitarth Thakkar <122635451+HitarthThakkar@users.noreply.github.com>
2025-09-13 17:20:02 +0530

    Time: 420 ms (72.44%), Space: 0B (100%) - LeetHub

---
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -0,0 +1,10 @@
+-- OFFSET is not getting hilighted but works now i see
+
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+SET N = N-1;
+  RETURN (
+      SELECT DISTINCT(salary) from Employee order by salary DESC
+      LIMIT 1 OFFSET N
+  );
+END
