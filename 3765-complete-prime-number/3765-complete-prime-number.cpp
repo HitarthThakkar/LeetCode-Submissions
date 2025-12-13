caad6ea29f83f77abc1b1282b9479e4d9c0816be
Hitarth Thakkar <122635451+HitarthThakkar@users.noreply.github.com>
2025-12-13 11:31:20 +0530

    Time: 207 ms (5.47%), Space: 8.2 MB (82.96%) - LeetHub

---
+++ 3765-complete-prime-number/3765-complete-prime-number.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+
+    bool isPrime(long long int n)
+    {
+        cout << n << endl;
+        if(n == 1) return false;
+        for(int i = 2; i < n; i++)
+            {
+                if(n % i == 0) return false;
+            }
+        return true;
+    }
+    
+    bool completePrime(int num) {
+        long long int n = 0;
+        long long int numcp = num;
+        long long int mul = 1;
+        while(numcp)
+        {
+            n += (numcp % 10) * mul;
+            numcp /= 10; mul *= 10;
+            // isPrime(n);
+            if(!isPrime(n)) return false;
+        }
+        string nn = to_string(num);
+        reverse(nn.begin(), nn.end());
+        n = 0, numcp = stoi(nn), mul = 1;
+
+        while(numcp)
+        {
+            n *= 10;
+            n += (numcp % 10);
+            numcp /= 10;
+            // isPrime(n);
+            if(!isPrime(n)) return false;
+        }
+
+        return true;
+    }
+};
