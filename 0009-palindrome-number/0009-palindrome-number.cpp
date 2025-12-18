ac9318b18a46b2d2bec7f30d926b69d6e2bbee62
Hitarth Thakkar <122635451+HitarthThakkar@users.noreply.github.com>
2025-12-18 10:14:09 +0530

    Time: 1 ms (58.19%), Space: 8.5 MB (68.18%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x < 0) return false;
-
-        int xcp = x;
-        vector<int> v;
-        while(xcp != 0) {
-            v.push_back(xcp % 10);
-            xcp /= 10;
+        if(x<0) return false;
+        int n = x;
+        unsigned int sum=0;
+        while(n!=0){
+            sum = sum*10 + n%10;
+            n /= 10;
         }
-
-        long long int revx = 0;
-        long long int mul = 1;
-        for(int j = v.size() - 1; j >= 0; j--) {
-            revx += (v[j] * mul);
-            mul *= 10;
-        }
-
-        return (x == revx);
+        
+        return sum == x;
     }
 };
