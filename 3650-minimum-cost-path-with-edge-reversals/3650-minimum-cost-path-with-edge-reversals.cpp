c7c92fe878223546032f1639a8a376434efe10e5
Hitarth Thakkar <122635451+HitarthThakkar@users.noreply.github.com>
2026-02-11 12:17:07 +0530

    Time: 323 ms (38.66%), Space: 315.9 MB (40.7%) - LeetHub

--- 3650-minimum-cost-path-with-edge-reversals/3650-minimum-cost-path-with-edge-reversals.cpp
+++ 3650-minimum-cost-path-with-edge-reversals/3650-minimum-cost-path-with-edge-reversals.cpp
@@ -3,9 +3,8 @@ class Solution
 public:
     int minCost(int n, vector<vector<int>>& edges)
     {
-        vector<vector<pair<int, int>>> adj(n), revadj(n);
+        vector<vector<pair<int, int>>> adj(n);
         vector<int> dis(n, 1e9);
-        vector<bool> used(n, false);
 
         priority_queue<
             pair<int,int>,
@@ -16,7 +15,7 @@ public:
         for(auto e : edges)
         {
             adj[e[0]].push_back({e[1], e[2]});
-            revadj[e[1]].push_back({e[0], e[2]});
+            adj[e[1]].push_back({e[0], 2 * e[2]});
         }
 
         dis[0] = 0;
@@ -28,21 +27,7 @@ public:
             int node = pq.top().second;
             pq.pop();
 
-            if(node == n - 1) continue;
-
-            if(!used[node])
-            {
-                for(auto nei : revadj[node])
-                {
-                    if(dis[nei.first] > dtn + (2 * nei.second))
-                    {
-                        dis[nei.first] = dtn + (2 * nei.second);
-                        pq.push({dis[nei.first], nei.first});
-                    }
-                }
-
-                used[node] = true;
-            }
+            if(node == n - 1) return dtn;
 
             for(auto nei : adj[node])
             {
