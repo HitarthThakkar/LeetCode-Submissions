bc23d464e0a7834d6a3361e711ce9b684f1a835e
Hitarth Thakkar <122635451+HitarthThakkar@users.noreply.github.com>
2026-03-08 22:15:53 +0530

    Time: 11 ms (23.05%), Space: 20 MB (41.72%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -5,53 +5,21 @@ public:
     {
         int n = numCourses;
         vector<vector<int>> adj(n);
-
-        for(auto ele : prerequisites)
-        {
-            adj[ele[0]].push_back(ele[1]);
-        }
-
+        for(auto ele : prerequisites) adj[ele[1]].push_back(ele[0]);
         vector<int> inDegree(n, 0);
-
-        for(int i = 0; i < n; i++)
-        {
-            for(auto it : adj[i])
-            {
-                inDegree[it]++;
-            }
-        }
-
+        for(int i = 0; i < n; i++) for(auto it : adj[i]) inDegree[it]++;
         queue<int> q;
-
-        for(int i = 0; i < n; i++)
-        {
-            if(inDegree[i] == 0)
-            {
-                q.push(i);
-            }
-        }
-
+        for(int i = 0; i < n; i++) if(inDegree[i] == 0) q.push(i);
         int ct = 0;
-
-        while(!q.empty())
-        {
+        while(!q.empty()) {
             int node = q.front();
-
             q.pop();
-
             ct++;
-
-            for(auto it : adj[node])
-            {
+            for(auto it : adj[node]) {
                 inDegree[it]--;
-
-                if(inDegree[it] == 0)
-                {
-                    q.push(it);
-                }
+                if(inDegree[it] == 0) q.push(it);
             }
         }
-
         return (ct == n);
     }
 };
